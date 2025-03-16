ed18ec1b7f869d519fe21ea3ef2eae1b57ee0216
Ihor Berezhnyi <ihorberezhnoi@gmail.com>
2025-03-16 17:55:14 +0200

    cors

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: 'http://localhost:5173',
+    origin: 'http://34.36.228.114', // Allow requests from this IP address
     methods: 'GET,POST',
     allowedHeaders: 'Content-Type, Authorization',
   });
